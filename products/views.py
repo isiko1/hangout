71758c7ca5df8f4b7c3112829f0bb3a6ed4f065a
isiko1 <lutersweg@hotmail.com>
2020-07-21 21:56:36 +0000

    added sorting JS

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Department
 # Create your views here.
 
@@ -21,32 +22,30 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'department':
+                sortkey = 'department__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
 
-
         if 'department' in request.GET:
             departments = request.GET['department'].split(',')
             products = products.filter(department__name__in=departments)
             departments = Department.objects.filter(name__in=departments)
 
-
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request,
-                               "You didn't enter any search criteria!")
+                messages.error(request, "No search criteria entered!")
                 return redirect(reverse('products'))
 
             queries = Q(name__icontains=query) | Q(brand__icontains=query)
             products = products.filter(queries)
-    
+
     current_sorting = f'{sort}_{direction}'
-    
+
     context = {
         'products': products,
         'search_term': query,
