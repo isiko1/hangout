88b5a5547c6e43b2b4e633646c55c3eeda17766f
isiko1 <lutersweg@hotmail.com>
2020-07-27 23:15:10 +0000

    Added checkout app and models

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('checkout/', views.checkout, name='checkout'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,9 +6,3 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
-
-
-def checkout(request):
-    """ A view to the checkout page """
-    context = {}
-    return render(request, 'home/checkout.html', context)
