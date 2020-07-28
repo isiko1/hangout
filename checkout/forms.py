60bc8b0e49b9e5f24ebcb24f030c38084555ae13
isiko1 <lutersweg@hotmail.com>
2020-07-28 22:35:58 +0000

    added checkout views and templates

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,29 +1,22 @@
 from django import forms
-from django_countries.fields import CountryField
+from .models import Order
 
 
-class CheckoutForm(forms.Form):
-    street_address = forms.CharField()
-    apartment_address = forms.CharField(required=False)
-    country = CountryField(blank_label='(select country)')
-    postcode = forms.CharField()
-    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
-    save_info = forms.BooleanField(widget=forms.CheckboxInput())
-    payment_option = forms.BooleanField(widget=forms.RadioSelect())
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
 
-
-"""
-Code from mini project
-"""
-
-
-def __init__(self, *args, **kwargs):
-    """
+    def __init__(self, *args, **kwargs):
+        """
         Add placeholders and classes, remove auto-generated
         labels and set autofocus on first field
         """
-    super().__init__(*args, **kwargs)
-    placeholders = {
+        super().__init__(*args, **kwargs)
+        placeholders = {
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
@@ -35,12 +28,12 @@ def __init__(self, *args, **kwargs):
             'county': 'County',
         }
 
-    self.fields['full_name'].widget.attrs['autofocus'] = True
-    for field in self.fields:
-        if self.fields[field].required:
-            placeholder = f'{placeholders[field]} *'
-        else:
-            placeholder = placeholders[field]
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
