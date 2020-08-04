51eb7ad666b6a3922e479c20bec8f13958e28ea3
isiko1 <lutersweg@hotmail.com>
2020-08-04 23:43:21 +0000

    Added product functionanlity and product form

---
+++ products/forms.py
@@ -0,0 +1,20 @@
+# Code modified from mini project
+
+from django import forms
+from .models import Product, Department
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        departments = Department.objects.all()
+        friendly_names = [(d.id, d.get_friendly_name()) for d in departments]
+
+        self.fields['department'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
