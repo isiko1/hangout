1bceab4a875d0213b9c732e84eda000dd3495b57
isiko1 <lutersweg@hotmail.com>
2020-08-06 00:57:37 +0000

    amended typo

--- hangout/settings.py
+++ hangout/settings.py
@@ -191,6 +191,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media',)
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
+
     AWS_STORAGE_BUCKET_NAME = 'hangout-4'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -198,12 +199,14 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
+
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
+    
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
